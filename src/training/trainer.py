9a22093e83c6cda00d9e012cd9a4e6b10c679c13
Boris Zhestiankin <zhestyankin.ba@phystech.edu>
2021-02-21 03:26:33 +0300

    no gpus locally

--- src/training/trainer.py
+++ src/training/trainer.py
@@ -4,7 +4,7 @@ from pytorch_lightning.callbacks import EarlyStopping
 from src.data.constants import EPOCHS
 
 
-def get_trainer():
+def get_trainer(on_colab=True):
     early_stop_callback = EarlyStopping(
         monitor="val_loss",
         min_delta=0.0,
@@ -14,7 +14,7 @@ def get_trainer():
     )
 
     trainer = Trainer(
-        gpus=1,
+        gpus=1 if on_colab else None,
         checkpoint_callback=False,
         accumulate_grad_batches=10,
         max_epochs=EPOCHS,

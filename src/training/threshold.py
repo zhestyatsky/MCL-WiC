fa5c9f99a5172e6207c98d2937d92f1e0e184fd1
Boris Zhestiankin <zhestyankin.ba@phystech.edu>
2021-02-22 17:59:15 +0300

    no threshold search for linear layer model;

--- src/training/threshold.py
+++ src/training/threshold.py
@@ -3,13 +3,20 @@ import torch
 from pytorch_lightning.metrics import Accuracy
 from sklearn.metrics import roc_curve
 
+from src.training.config import get_config
 
-def get_threshold_with_acc(labels, proba):
-    fpr, tpr, thlds = roc_curve(labels, proba)
-    optimal_idx = np.argmax(tpr - fpr)
-    thld = thlds[optimal_idx]
+
+def find_threshold_if_needed(labels, proba, model_description):
+    model_config = get_config(model_description)
+    thld = 0.5
+
+    if model_config["top"] == "cosine_similarity":
+        fpr, tpr, thlds = roc_curve(labels, proba)
+        optimal_idx = np.argmax(tpr - fpr)
+        thld = thlds[optimal_idx]
+
     y_pred = (proba > thld).float()
     acc = Accuracy()
     acc_value = acc(y_pred, torch.tensor(labels)).item()
-    print("Threshold: {}, Accuracy: {}".format(thld, acc_value))
+    print("Threshold: {}, Validation Accuracy: {}".format(thld, acc_value))
     return thld, acc_value

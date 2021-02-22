2c6121c79a0b497f6320d638283e943b81e17754
Boris Zhestiankin <zhestyankin.ba@phystech.edu>
2021-02-22 15:16:32 +0300

    change testing structure

--- src/testing/testing.py
+++ src/testing/testing.py
@@ -1,12 +1,25 @@
 import json
 
 
-def get_predictions(proba, thld):
-    return proba > thld
+def _get_predictions(probas, thld):
+    return probas > thld
 
 
-def generate_output(predictions, ids, path):
-    result = [{"id": id, "tag": "T"} if pred else {"id": id, "tag": "F"} for id, pred in
-              zip(ids, predictions)]
-    with open(path, 'w') as f:
-        f.write(json.dumps(result))
+def get_outputs(probas, thld, ids):
+    preds = _get_predictions(probas, thld)
+    probas_output = [{"id": id, "proba": proba} for id, proba in zip(ids, probas)]
+    preds_output = [{"id": id, "tag": "T" if pred else "F"} for id, pred in zip(ids, preds)]
+    return probas_output, preds_output
+
+
+def dump_outputs(probas, thld, ids, output_dir):
+    probas_path = output_dir + 'proba.json'
+    preds_path = output_dir + 'preds.json'
+
+    probas_output, preds_output = get_outputs(probas, thld, ids)
+
+    with open(probas_path, 'w') as f:
+        f.write(json.dumps(probas_output))
+
+    with open(preds_path, 'w') as f:
+        f.write(json.dumps(preds_output))
